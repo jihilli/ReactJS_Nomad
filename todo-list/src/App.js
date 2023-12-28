6877ad74d64760b40465a0b95893a55726cac5dc
jeehee <jeehee2610@gmail.com>
2023-12-29 00:45:36 +0900

    Simple ToDo List w/ React

---
+++ todo-list/src/App.js
@@ -0,0 +1,36 @@
+import { useState } from "react";
+
+function App() {
+    const [toDo, setToDo] = useState("");
+    const [toDos, setToDos] = useState([]); // toDo를 여러개 받아줄 배열
+    const onChange = (e) => setToDo(e.target.value);
+    const onSubmit = (e) => {
+        e.preventDefault();
+        if (toDo === "") {
+            return;
+        }
+        setToDos((currentArray) => [toDo, ...currentArray]); // ...currentArray : 해당 배열 안의 element들을 반환함
+        // 현재의 toDo를 받아와서, 새로운 array로 리턴함
+        setToDo("");
+    };
+    console.log(toDos);
+    return (
+        <div>
+            <h1>My To Dos ({toDos.length})</h1>
+            <form onSubmit={onSubmit}>
+                <input onChange={onChange} value={toDo} type="text" placeholder="Wrtie your to do..." />
+                <button>Add To Do</button>
+            </form>
+            <hr />
+            <ul>
+                {toDos.map((item, index) => (
+                    <li key={index}>{item}</li>
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+export default App;
+
+// map함수 : array에 있는 item을 내가 원하는 무엇이든지로 바꿔주는 역할을 함

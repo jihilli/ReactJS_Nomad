45545aa62d27abd8e147abf762500ec6e2022bec
jeehee <jeehee2610@gmail.com>
2024-01-10 15:33:59 +0900

    Movie 2 - using React Router

---
+++ practice-react/src/routes/Home.js
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from "react";
+
+import Movie from "../components/Movie";
+
+const Home = () => {
+    const [loading, setLoading] = useState(true);
+    const [movies, setMovies] = useState([]);
+    const getMovies = async () => {
+        // const response = await fetch(`https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year`);
+        // const json = await response.json();
+        const json = await (
+            await fetch(`https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year`)
+        ).json();
+        setMovies(json.data.movies);
+        setLoading(false);
+    };
+    useEffect(() => {
+        getMovies();
+    }, []);
+
+    /*
+    useEffect(() => {
+        fetch(`https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year`)
+            .then((response) => response.json())
+            .then((json) => {
+                setMovies(json.data.movies);
+                setLoading(false);
+            });
+    }, []);
+    console.log(movies);
+    */
+    return (
+        <div>
+            {loading ? (
+                <h1>Loading...</h1>
+            ) : (
+                <div>
+                    {movies.map((movie) => (
+                        <Movie
+                            key={movie.id}
+                            id={movie.id}
+                            coverImg={movie.medium_cover_image}
+                            title={movie.title}
+                            summmary={movie.summary}
+                            genres={movie.genres}
+                        />
+                    ))}
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default Home;
